07cbf2775af172df3fb2272b10ffe2ede0040947
Pohl <linux-pohl-v2@ramita.168.0.9>
2023-06-02 11:21:32 -0500

    I start working on the TDODEs for the density matrix

---
+++ Code/Dynamics_Equation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <complex>
+#include <boost/numeric/odeint.hpp>
+
+
+typedef std::complex<double> state_type;
+
+//QDDM stands for Quantum Dynamics of Density Matrix
+struct QDDM
+{
+
+
+
+};
+
+
+int main(void)
+{
+  std::cout << "Hallo" << std::endl;
+
+  return 0;
+}
